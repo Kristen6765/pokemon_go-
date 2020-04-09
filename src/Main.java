cc6ace8c648d267652dc4248220e83108ff0ee22
kristenpeng <jiahui.peng@mail.mcgill.ca>
2020-04-08 23:13:16 -0400

    main

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,7 @@ import java.util.Scanner;
 
 class connectExample {
         public static void main(String args[]) throws SQLException {
-            System.out.println("wWlcome to pokemongo!");
+            System.out.println("Welcome to pokemongo!");
             System.out.println("Are you a new user? If yes plase press 0 to create a new account.");
             Scanner scr = new Scanner(System.in);
             int newPlayer = scr.nextInt();
@@ -64,7 +64,7 @@ class connectExample {
                 break;
             case 3:
                 // code block
-                addGroup(sqlCode,sqlState);
+
                 break;
             case 4:
                 // check bag
@@ -89,24 +89,22 @@ class connectExample {
         String passwordstring="Dalao2020.Dalao2020@";
         Connection con = DriverManager.getConnection (url,usernamestring, passwordstring) ;
         Statement statement = con.createStatement ( ) ;
-
+        int maxCode=0;
         int maxCurrentID=-1;
         //get all code from Groups
         // Querying a table
         try {
-            String querySQL = "SELECT max(code) from groups";
+            String querySQL = "SELECT max(code) from groups;";
             System.out.println (querySQL) ;
             ResultSet rs = statement.executeQuery ( querySQL ) ;
-            //generate a new id that does not in the db
+            //create an unique code not in db yet
             while ( rs.next ( ) ) {
-                 maxCurrentID = rs.getInt (0) ;
-                System.out.println ("max is :  " + maxCurrentID);
-
+                 maxCode = rs.getInt (1) ;
+                System.out.println ("maxCode:  " + maxCode);
             }
             System.out.println ("DONE");
         } catch (SQLException e)
         {
-
             sqlCode = e.getErrorCode(); // Get SQLCODE
             sqlState = e.getSQLState(); // Get SQLSTATE
 
@@ -114,13 +112,30 @@ class connectExample {
             // something more meaningful than a print would be good
             System.out.println("Code: " + sqlCode + "  sqlState: " + sqlState);
         }
-       int newCode= maxCurrentID+1;
 
-        //add this code to table group first
-        // Inserting Data into the table (befriends)
+        int newCode= maxCode+1;
+
+        try {
+            String insertSQL = "INSERT INTO  groups VALUES ("+newCode+")" ;
+            System.out.println (insertSQL) ;
+            statement.executeUpdate (insertSQL) ;
+            System.out.println ("DONE");
+        } catch (SQLException e)
+        {
+            sqlCode = e.getErrorCode(); // Get SQLCODE
+            sqlState = e.getSQLState(); // Get SQLSTATE
+            System.out.println("Code: " + sqlCode + "  sqlState: " + sqlState);
+        }
+
+
+        //get name from user and add name and code to table Players
+        System.out.println("Hi, I'll help you to create a new account, please create a new name.");
+        Scanner userChoice = new Scanner(System.in);
+        String name = userChoice.nextLine();
+        //magic door to the method
         try {
 
-            String insertSQL = "INSERT INTO  groups VALUES ("+newCode+")" ;
+            String insertSQL = "INSERT INTO  players VALUES (\'"+name+"\',0,\'"+newCode+"\')" ;
             System.out.println ( insertSQL ) ;
             statement.executeUpdate ( insertSQL ) ;
             System.out.println ( "DONE" ) ;
@@ -133,12 +148,6 @@ class connectExample {
             // something more meaningful than a print would be good
             System.out.println("Code: " + sqlCode + "  sqlState: " + sqlState);
         }
-
-
-
-
-        //get name from user and add name and code to table Players
-        System.out.println("Hi, I'll help you to create a new account, please create a new name.");
 
     }
 
@@ -168,10 +177,7 @@ class connectExample {
 
         // Inserting Data into the table (befriends)
         try {
-
-            //Drew Ima2
-//Wendy Ima
-            String insertSQL = "INSERT INTO  befriends(friends_username, my_username) VALUES ("+friendName+","+ userName +") " ;
+            String insertSQL = "INSERT INTO  befriends(friends_username, my_username) VALUES (\'"+friendName+"\',\'"+ userName +"\') " ;
             System.out.println ( insertSQL ) ;
             statement.executeUpdate ( insertSQL ) ;
             System.out.println ( "DONE" ) ;
@@ -191,37 +197,7 @@ class connectExample {
 
 
         //print out all friends of user
-        System.out.println("now you are friend with: ");
-        // Querying a table
-            try {
-                System.out.println(userName);
-                String querySQL = "SELECT friends_username from " + "befriends where username= '" + userName +"'";
-
-                statement=con.prepareStatement(querySQL);
-
-                ((PreparedStatement) statement).setString(1, friendName);
-
-                System.out.println (querySQL) ;
-
-                ResultSet rs = statement.executeQuery ( querySQL ) ;
-               // System.out.println("1");
-                while ( rs.next ( ) ) {
-                //    System.out.println("2");
-                    String friends_username = rs.getString (0) ;
-                    System.out.println ("my_usernmae:  " + friends_username);
-
-                }
-                System.out.println ("DONE");
-            } catch (SQLException e)
-            {
-
-                sqlCode = e.getErrorCode(); // Get SQLCODE
-               sqlState = e.getSQLState(); // Get SQLSTATE
-
-                // Your code to handle errors comes here;
-                // something more meaningful than a print would be good
-                System.out.println("Code: " + sqlCode + "  sqlState: " + sqlState);
-            }
+        System.out.println("now you are friend with: "+friendName);
 
         statement.close ( ) ;
         con.close ( ) ;
@@ -249,8 +225,8 @@ class connectExample {
             // String userName="Alice";
             // Querying a table
             try {
-                String querySQL = "SELECT pokename from " + "capturablepokemons where username= '" + userName +"'";
-
+                String querySQL = "SELECT pokename from capturablepokemons where username= '" + userName +"'";
+               // String querySQL = "SELECT max(code) from groups;";
                 System.out.println (querySQL) ;
                 ResultSet rs = statement.executeQuery ( querySQL ) ;
 
@@ -279,25 +255,7 @@ class connectExample {
 
     }
 
-    /**
-     * if group contain less than 20 members, the user canbe added to the group
-     * @param sqlCode
-     * @param sqlState
-     * @throws SQLException
-     */
-    static void  addGroup(int sqlCode,String sqlState) throws SQLException {
-        // This is the url you must use for Postgresql.
-        //Note: This url may not valid now !
-        String url = "jdbc:postgresql://comp421.cs.mcgill.ca:5432/cs421";
-        String usernamestring="cs421g36";
-        String passwordstring="Dalao2020.Dalao2020@";
-        Connection con = DriverManager.getConnection (url,usernamestring, passwordstring) ;
-        Statement statement = con.createStatement ( ) ;
 
-
-
-
-    }
 
 }
 
